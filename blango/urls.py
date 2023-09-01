4110475fc03e53d41a651b7faf9e6e431b58571f
Hooman Khoshdel <hooman.khoshdel@gmail.com>
2023-09-01 09:46:26 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
