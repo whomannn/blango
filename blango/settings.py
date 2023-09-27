52e4af46aba5a9d0e9df82b783d32b9933fd55c8
Hooman Khoshdel <hooman.khoshdel@gmail.com>
2023-09-27 10:12:43 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -44,6 +44,7 @@ class Dev(Configuration):
       "crispy_forms",
       "crispy_bootstrap5",
       "debug_toolbar",
+      "rest_framework",
   ]
 
   MIDDLEWARE = [
--- blog/api_views.py
+++ blog/api_views.py
@@ -5,38 +5,37 @@ from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-
+from blog.api.serializers import PostSerializer
 from blog.models import Post
 
 
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
+
+
 
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+        return JsonResponse(PostSerializer(post,many = true).data)
     elif request.method == "POST":
         post_data = json.loads(request.body)
-        post = Post.objects.create(**post_data)
+        serializer = PostSerializer(date=post_data)
+        serializer.is_valid(raise_exceptions=True)
+        post = serializer.save()
         return HttpResponse(
             status=HTTPStatus.CREATED,
             headers={"Location": reverse("api_post_detail", args=(post.pk,))},
         )
-
+    elif request.method == "PUT":
+      post_data = json.loads(request.body)
+      serializer = PostSerializer(post, data=post_data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return HttpResponse(
+            status=HTTPStatus.CREATED,
+            headers={"Location": reverse("api_post_detail", args=(post.pk,))},
+        )
     return HttpResponseNotAllowed(["GET", "POST"])
 
 
