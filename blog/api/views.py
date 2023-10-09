2f2d158f5e899246b1217def7d06d854f9914986
Hooman Khoshdel <hooman.khoshdel@gmail.com>
2023-10-09 07:44:23 +0000

    Finish nested relationships

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,8 @@
 from rest_framework import generics
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blog.models import Post
 from django.contrib.auth.models import User
-from blog.api.serializers import PostSerializer, UserSerializer
 
 class PostList(generics.ListCreateAPIView):
   queryset=Post.objects.all()
@@ -12,7 +11,7 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
